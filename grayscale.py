1604ef3350ef5ea872fdd1794b5454caa6814108
ayeshafalak <60551252+ayeshafalak@users.noreply.github.com>
2020-08-04 20:45:43 +0530

    Add files via upload

---
+++ grayscale.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
+import cv2
+
+
+# In[22]:
+
+
+img=cv2.imread('pictures/Jellyfish.jpg',0)
+print(img)
+
+
+# In[23]:
+
+
+cv2.imshow('image',img)
+cv2.waitKey(6000)
+cv2.destroyAllWindows()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
