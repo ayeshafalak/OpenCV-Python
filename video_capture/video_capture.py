311f6f466d1033cfbb6e2682b1a205c017c743f3
ayeshafalak <60551252+ayeshafalak@users.noreply.github.com>
2020-08-06 20:32:40 +0530

    Create video_capture.py

---
+++ video_capture/video_capture.py
@@ -0,0 +1,13 @@
+import cv2,time
+cap=cv2.VideoCapture(0)
+print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+print("Press q to Quit")
+while(True):
+    ret, frame = cap.read() 
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
