c07c4ab01d1c040ff62602573ffb647693ab9007
ayeshafalak <60551252+ayeshafalak@users.noreply.github.com>
2020-08-06 20:33:49 +0530

    Create add_images.py

---
+++ add_images/add_images.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2
+img1=cv2.imread('Pictures/flower.jpg')
+img2=cv2.imread('Pictures/Jellyfish.jpg')
+img1=cv2.resize(img1,(512,512))
+img2=cv2.resize(img2,(512,512))
+result=cv2.add(img1,img2);
+result2=cv2.addWeighted(img1,.2,img2,.8,0);
+cv2.imshow('result',result)
+cv2.imshow('result2',result2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
