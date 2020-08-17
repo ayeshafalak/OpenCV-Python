ecacd3162ff70e173c8b55173d32ff72f0e1f1c6
ayeshafalak <60551252+ayeshafalak@users.noreply.github.com>
2020-08-17 14:11:02 +0530

    Update grayscale.py

--- gray_img/grayscale.py
+++ gray_img/grayscale.py
@@ -2,8 +2,9 @@
 # coding: utf-8
 
 import cv2                                      #import the opencv package
-img=cv2.imread('Jellyfish.jpg',0)      #read the image using this function
-print(img)                                      #printing the matrix of image in grayscale
-cv2.imshow('image',img)                         #displaying the grayscale image in a new window named 'image'
+img=cv2.imread('Jellyfish.jpg',0)               #read the image using this function
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+print(gray)                                      #printing the matrix of image in grayscale
+cv2.imshow('image',gray)                         #displaying the grayscale image in a new window named 'image'
 cv2.waitKey(6000)                               #the window will appear for 6s
 cv2.destroyAllWindows()                         #destroys the window
